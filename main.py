ad6b8c72ba75df4783521b1c30e3f5d40995c567
R-IGO-PROJECT <pruebadeproyectos1@gmail.com>
2026-07-21 12:01:16 -0600

    fix/arreglar en main.py el bucle while, incluir un try-except para la variable opc
    modifique el valor de flag para el bucle while, dandoles valores True y False, ademas de que tambien se agrego los inicios de un try-except para en caso de que la variable opc se le asigne un valor que no sea entero

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from paquete_imprimir.modulo_imprimir_menu import *
-flag = 1
-while flag == 1:
+flag = True
+while flag:
     imprimir_menu()
     opc = input()
+    try:
+        opc = int(opc)
+    except ValueError:
+        
     match opc:
         case 0:
             imprimir_despedida()
-            flag = 0
+            flag = False
         case 1:
-            
             pass
         case 2:
             pass
