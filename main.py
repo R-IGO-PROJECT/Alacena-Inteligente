f9604d279962affca062ffaaa371753ffa7f5b7d
R-IGO-PROJECT <pruebadeproyectos1@gmail.com>
2026-07-21 10:48:22 -0600

    feature/agregar prints para el menu principal, nueva funcion para imprimir despedida y agregar el match case en main.py
    en el paquete paquete_imprimir agregue la funcion para imprimir el menu principal con todas las opciones que requiere para que el usuario las pueda hacer, dentro de ese mismo paquete se agrego un nuevo modulo para imprimir una despedida. Dentro de main.py agregamos la funcion de match case para cada opcion del menu y tambien agregamos para ya poder usar los modulos de menu y despedida.

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from paquete_imprimir.modulo_imprimir_menu import *
-
+flag = 1
 while flag == 1:
-    print("Bienvenido al control de Insumos, que es lo que desea hacer?")
+    imprimir_menu()
+    opc = input()
+    match opc:
+        case 0:
+            imprimir_despedida()
+            flag = 0
+        case 1:
+            
+            pass
+        case 2:
+            pass
+        case 3:
+            pass
+        case 4:
+            pass
+
     
--- paquete_imprimir/modulo_imprimir_menu.py
+++ paquete_imprimir/modulo_imprimir_menu.py
@@ -1,2 +1,11 @@
 def imprimir_menu():
-    pass
+    print("Bienvenido al control de Insumos, que es lo que desea hacer?")
+    print("1.- Registrar un producto")
+    print("2.- Modificar un producto")
+    print("3.- Borrar un producto")
+    print("4.- Ver productos existentes")
+    print("0.- Salir")
+    print("Introduzca un numero de la opcion que quiera hacer: ")
+
+def imprimir_despedida():
+    print("Muchas gracias, vuelva pronto")
