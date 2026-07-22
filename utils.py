a5866257733dd2f0be126eca912e9229bf5282bf
R-IGO-PROJECT <pruebadeproyectos1@gmail.com>
2026-07-21 19:19:12 -0600

    feature/agregar en utils.py nueva validacion
    en el archivo de utils.py agregue una funcion que es para validar respuestas que quiera dar el usuario que sean strings, para que no deje en blanco ese espacio de respuesta e ingrese por lo menos un caracter valido.

--- utils.py
+++ utils.py
@@ -19,3 +19,14 @@ def validar_decimal():
         except ValueError:
             print("El numero que ingreso no es un numero decimal o entero, intentelo de nuevo")
     return numero_decimal
+
+def validar_string():
+    flag = True
+    while flag:
+        respuesta_string = input("Ingrese su respuesta: ")
+        validar_respuesta_vacia = respuesta_string.strip()
+        if validar_respuesta_vacia == "":
+            print("Ingrese de nuevo una respuesta valida y que no sea vacia")
+        else:
+            return validar_respuesta_vacia
+            
