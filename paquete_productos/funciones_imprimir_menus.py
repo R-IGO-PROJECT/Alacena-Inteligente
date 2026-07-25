f76d8f5dc967b662c1a22ef171a08ac08ab1f3eb
R-IGO-PROJECT <pruebadeproyectos1@gmail.com>
2026-07-25 02:26:00 -0600

    feature/creacion de archivo funciones_imprimir_menus
    Se creo el archivo imprimir menus dentro del paquete paquete_productos que contiene los modulos con prints para diferentes menus o avisos al usuario.

---
+++ paquete_productos/funciones_imprimir_menus.py
@@ -0,0 +1,68 @@
+def imprimir_continuar_registro():
+    print("Menu para registrar un nuevo producto")
+    print("1.- Continuar registrando un producto")
+    print("0.- Salir al menu principal")
+
+def imprimir_tipos_de_producto():
+    print("De que tipo de producto es el producto que quiere registrar?: ")
+    print("1.- Producto solido contable por unidades")
+    print("2.- Producto solido a granel (gramos)")
+    print("3.- Producto liquido (mililitros)")
+    print("0.- Cancelar y salir")
+
+def cancelando():
+    print("Cancelando acción...")
+
+def imprimir_categorias_de_productos():
+    print("Registrando un Producto por Unidad")
+    print("Introduzca la categoria de su producto: ")
+    print("1.- Abarrotes / Despensa")
+    print("2.- Lacteos y refrigerados")
+    print("3.- Frutas y verduras")
+    print("4.- Carnes, aves y pescados")
+    print("5.- Limpieza del hogar")
+    print("6.- Higiene personal / Baño")
+    print("7.- Bebidas")
+    print("8.- Otros")
+
+def imprime_pregunta_del_nombre():
+    print("Ingrese el nombre del producto: ")
+
+def imprime_pregunta_si_compro_paquetes():
+    print("Compro paquetes que contienen varias unidades de el producto?")
+    print("1.- Si")
+    print("2.- No")
+
+def imprime_pregunta_de_paquetes():
+    print("Ingrese el numero de paquetes que quiere registrar: ")
+
+def imprime_pregunta_de_unidades_por_paquete():
+    print("Ingrese el numero de unidades que hay por paquete: ")
+
+def imprime_pregunta_numero_de_unidades_solas():
+    print("Ingrese el numero de unidades que compro del producto: ")
+
+def imprime_pregunta_cantidad_si_en_gr_o_kg():
+    print("En que unidad va a ingresar la cantidad del producto, gramos o kilogramos?")
+    print("1.- gramos")
+    print("2.- kilogramos")
+
+def imprime_pregunta_cantidad_gr():
+    print("Ingrese el numero de gramos que contiene la unidad/unidades:")
+
+def imprime_pregunta_cantidad_kg():
+    print("Ingrese el numero de kilogramos que contiene la unidad/unidades")
+
+def imprime_pregunta_si_ml_o_L():
+    print("En que unidad va a ingresar la cantidad del producto, mililitros o litros?")
+    print("1.- mililitros")
+    print("2.- litros")
+
+def imprime_pregunta_cantidad_ml():
+    print("Ingrese el numero de mililitros que contiene la unidad/unidades: ")
+
+def imprime_pregunta_cantidad_L():
+    print("Ingrese el numero de litros que contiene la unidad/unidades: ")
+
+def confirmacion_producto_registrado():
+    print("Producto guardado exitosamente")
