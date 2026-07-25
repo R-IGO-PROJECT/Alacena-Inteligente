c2aae7e8beeafbef8da1a1cff5c78749f096a8b6
R-IGO-PROJECT <pruebadeproyectos1@gmail.com>
2026-07-25 02:24:11 -0600

    fix/arreglo en paquete_imprimir archivo modulo_imprimir_menu
    Un pequeño arreglo al archivo modulo_imprimir_menu, solo le quite los 2 puntos finales a la parte del print introducir lo que quiera hacer el usuario

--- paquete_imprimir/modulo_imprimir_menu.py
+++ paquete_imprimir/modulo_imprimir_menu.py
@@ -5,7 +5,7 @@ def imprimir_menu():
     print("3.- Borrar un producto")
     print("4.- Ver productos existentes")
     print("0.- Salir")
-    print("Introduzca un numero de la opcion que quiera hacer: ")
+    print("Introduzca un numero de la opcion que quiera hacer")
 
 def imprimir_despedida():
     print("Muchas gracias, vuelva pronto")
