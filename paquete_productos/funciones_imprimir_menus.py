af6dcab2c93af208c55e6362b369ea977a27870a
R-IGO-PROJECT <pruebadeproyectos1@gmail.com>
2026-07-31 17:12:25 -0600

    feature/funciones para imprimir mensajes
    Se implementaron funciones que se van a usar para la funcion de borrar productos por medio de busqueda, incluyen mensajes como para mostrar un menu y mensajes para indicaciones para el usuario.

--- paquete_productos/funciones_imprimir_menus.py
+++ paquete_productos/funciones_imprimir_menus.py
@@ -3,6 +3,11 @@ def imprimir_continuar_registro():
     print("1.- Continuar registrando un producto")
     print("0.- Salir al menu principal")
 
+def imprimir_continuar_borrando_producto():
+    print("Menu para borrar un producto")
+    print("1.- Continuar borrando un producto")
+    print("0.- Salir al menu principal")
+
 def imprimir_tipos_de_producto():
     print("De que tipo de producto es el producto que quiere registrar?: ")
     print("1.- Producto solido contable por unidades")
@@ -66,3 +71,67 @@ def imprime_pregunta_cantidad_L():
 
 def confirmacion_producto_registrado():
     print("Producto guardado exitosamente")
+
+def imprime_pregunta_mostrar_todo_el_inventario_o_ir_a_borrar_por_busqueda():
+    print("Ingrese la opción que quiera realizar:")
+    print("1.- Mostrar todo el inventario")
+    print("2.- Borrar por ID del producto")
+    print("3.- Borrar por busqueda del nombre")
+    print("4.- Borrar por busqueda de categoria")
+    print("0.- Cancelar")
+
+def imprime_pregunta_ID_a_borrar():
+    print("Ingrese el ID del producto que quiera borrar:")
+
+def imprime_pregunta_nombre_a_borrar():
+    print("Ingrese el nombre del producto que quiera borrar:")
+
+def imprime_pregunta_categoria_a_borrar():
+    print("Ingrese la categoria del producto que quiera borrar:")
+
+def imprime_pregunta_ID_a_borrar_confirmacion():
+    print("Confirme el ID del producto que quiera borrar:")
+
+def imprime_no_encontro_el_producto():
+    print("Producto no encontrado...")
+
+def imprime_si_encontro_el_producto():
+    print("Producto encontrado...")
+
+def imprime_mensaje_producto_seleccionado():
+    print("Este es el producto seleccionado:")
+
+def imprime_producto_eliminado():
+    print("Producto eliminado con exito")
+
+def pregunta_continuar_con_eliminar_el_producto():
+    print("Quiere continuar con la eliminación del producto?")
+    print("1.- Si")
+    print("2.- No, cancelar...")
+
+def pregunta_continuar_o_salir_ID_no_encontrado():
+    print("Desea continuar ingresando otro ID de producto o cancelar el proceso?:")
+    print("1.- Continuar")
+    print("2.- Salir")
+
+def pregunta_continuar_o_salir_nombre_no_encontrado():
+    print("Desea continuar ingresando otro nombre de producto o cancelar el proceso?:")
+    print("1.- Continuar")
+    print("2.- Salir")
+
+def pregunta_continuar_o_salir_categoria_no_encontrada():
+    print("Desea continuar ingresando otra categoria de producto o cancelar el proceso?:")
+    print("1.- Continuar")
+    print("2.- Salir")
+
+def imprime_siguiendo():
+    print("Siguiendo...")
+
+def imprime_mensaje_coincidencias_encontradas():
+    print("Estas son las coincidencias encontradas:")
+
+def imprime_mensaje_ID_no_valido():
+    print("El ID ingresado no esta entre las coincidencias, ingrese uno valido")
+
+def imprime_mensaje_nombre_no_valido():
+    print("El nombre ingresado no esta entre las coincidencias, ingrese uno valido")
