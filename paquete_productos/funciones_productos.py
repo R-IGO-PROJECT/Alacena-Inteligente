07b3813e4434cf02f8963dd94e05f640f7ca99ab
R-IGO-PROJECT <pruebadeproyectos1@gmail.com>
2026-07-31 17:14:08 -0600

    Update funciones_productos.py
    Se agregó la función para poder buscar un producto ya sea por ID, nombre o categoria que le haya asignado el usuario y que pueda seleccionar el producto deseado y eliminarlo o cancelar el proceso.

--- paquete_productos/funciones_productos.py
+++ paquete_productos/funciones_productos.py
@@ -137,4 +137,193 @@ def guardar_producto_liquido(categoria, nombre, numero_paquetes, unidades, liqui
 def mostrar_productos(inventario):
     for producto in inventario:
         producto.mostrar_producto()
-        
+
+def borrar_producto(inventario: list):
+    while True:
+        imprimir_continuar_borrando_producto()
+        opc = definir_rango_valido_numero_entero(0, 1)
+        match opc:
+            case 0:
+                return inventario
+            case 1:
+                while True:
+                    imprime_pregunta_mostrar_todo_el_inventario_o_ir_a_borrar_por_busqueda()
+                    opc = definir_rango_valido_numero_entero(0, 4)
+                    match opc:
+                        case 0:
+                            cancelando()
+                            return inventario
+                        case 1:
+                            mostrar_productos(inventario)
+                        case 2:
+                            while True:
+                                imprime_pregunta_ID_a_borrar()
+                                ID_borrar = validar_entero()
+                                ID_borrar_str = str(ID_borrar)
+                                coincidencias = []
+                                for producto in inventario:
+                                    id_producto_inventario = str(producto.id)
+                                    if ID_borrar_str in id_producto_inventario:
+                                        coincidencias.append(producto)
+                                if len(coincidencias) == 0:
+                                    imprime_no_encontro_el_producto()
+                                    pregunta_continuar_o_salir_ID_no_encontrado()
+                                    opc = definir_rango_valido_numero_entero(1, 2)
+                                    match opc:
+                                        case 1:
+                                            pass
+                                        case 2:
+                                            cancelando()
+                                            return inventario
+                                else:
+                                    imprime_mensaje_coincidencias_encontradas()
+                                    for coincidencia in coincidencias:
+                                        coincidencia.mostrar_producto()
+                                    while True:
+                                        imprime_pregunta_ID_a_borrar_confirmacion()
+                                        ID_borrar_confirmar = validar_entero()
+                                        lista_coincidencia_final = []
+                                        for coincidencia in coincidencias:
+                                            if coincidencia.id == ID_borrar_confirmar:
+                                                lista_coincidencia_final.append(coincidencia)
+                                        if len(lista_coincidencia_final) == 0:
+                                            imprime_mensaje_ID_no_valido()
+                                        else:
+                                            break
+                                    imprime_mensaje_producto_seleccionado()
+                                    lista_coincidencia_final[0].mostrar_producto()
+                                    pregunta_continuar_con_eliminar_el_producto()
+                                    opc = definir_rango_valido_numero_entero(1, 2)
+                                    match opc:
+                                        case 1:
+                                            for indice_producto, producto in enumerate(inventario):
+                                                if lista_coincidencia_final[0].id == producto.id:
+                                                    indice_borrar = indice_producto
+                                            inventario.pop(indice_borrar)
+                                            imprime_producto_eliminado()
+                                            return inventario
+                                        case 2:
+                                            pregunta_continuar_o_salir_ID_no_encontrado()
+                                            opc = definir_rango_valido_numero_entero(1, 2)
+                                            match opc:
+                                                case 1:
+                                                    pass
+                                                case 2:
+                                                    cancelando()
+                                                    return inventario
+                        case 3:
+                            while True:
+                                imprime_pregunta_nombre_a_borrar()
+                                nombre_borrar = validar_string()
+                                nombre_borrar = nombre_borrar.strip().lower()
+                                coincidencias = []
+                                for producto in inventario:
+                                    nombre_producto = producto.nombre
+                                    nombre_producto = nombre_producto.strip().lower()
+                                    if nombre_borrar in nombre_producto:
+                                        coincidencias.append(producto)
+                                if len(coincidencias) == 0:
+                                    imprime_no_encontro_el_producto()
+                                    pregunta_continuar_o_salir_nombre_no_encontrado()
+                                    opc = definir_rango_valido_numero_entero(1, 2)
+                                    match opc:
+                                        case 1:
+                                            pass
+                                        case 2:
+                                            cancelando()
+                                            return inventario
+                                else:
+                                    imprime_mensaje_coincidencias_encontradas()
+                                    for coincidencia in coincidencias:
+                                        coincidencia.mostrar_producto()
+                                    while True:
+                                        imprime_pregunta_ID_a_borrar_confirmacion()
+                                        ID_borrar_confirmar = validar_entero()
+                                        lista_coincidencia_final = []
+                                        for coincidencia in coincidencias:
+                                            if coincidencia.id == ID_borrar_confirmar:
+                                                lista_coincidencia_final.append(coincidencia)
+                                        if len(lista_coincidencia_final) == 0:
+                                            imprime_mensaje_nombre_no_valido()
+                                        else:
+                                            break
+                                    imprime_mensaje_producto_seleccionado()
+                                    lista_coincidencia_final[0].mostrar_producto()
+                                    pregunta_continuar_con_eliminar_el_producto()
+                                    opc = definir_rango_valido_numero_entero(1, 2)
+                                    match opc:
+                                        case 1:
+                                            for indice_producto, producto in enumerate(inventario):
+                                                if lista_coincidencia_final[0].id == producto.id:
+                                                    indice_borrar = indice_producto
+                                            inventario.pop(indice_borrar)
+                                            imprime_producto_eliminado()
+                                            return inventario
+                                        case 2:
+                                            pregunta_continuar_o_salir_nombre_no_encontrado()
+                                            opc = definir_rango_valido_numero_entero(1, 2)
+                                            match opc:
+                                                case 1:
+                                                    pass
+                                                case 2:
+                                                    cancelando()
+                                                    return inventario
+                        case 4:
+                            while True:
+                                imprime_pregunta_categoria_a_borrar()
+                                categoria_borrar = definir_rango_valido_numero_entero(1, 8)
+                                coincidencias = []
+                                for producto in inventario:
+                                    categoria_producto = producto.categoria
+                                    if categoria_borrar == categoria_producto:
+                                        coincidencias.append(producto)
+                                if len(coincidencias) == 0:
+                                    imprime_no_encontro_el_producto()
+                                    pregunta_continuar_o_salir_categoria_no_encontrada()
+                                    opc = definir_rango_valido_numero_entero(1, 2)
+                                    match opc:
+                                        case 1:
+                                            pass
+                                        case 2:
+                                            cancelando()
+                                            return inventario
+                                else:
+                                    imprime_mensaje_coincidencias_encontradas()
+                                    for coincidencia in coincidencias:
+                                        coincidencia.mostrar_producto()
+                                    while True:
+                                        imprime_pregunta_ID_a_borrar_confirmacion()
+                                        ID_borrar_confirmar = validar_entero()
+                                        lista_coincidencia_final = []
+                                        for coincidencia in coincidencias:
+                                            if coincidencia.id == ID_borrar_confirmar:
+                                                lista_coincidencia_final.append(coincidencia)
+                                        if len(lista_coincidencia_final) == 0:
+                                            imprime_mensaje_ID_no_valido()
+                                        else:
+                                            break
+                                    imprime_mensaje_producto_seleccionado()
+                                    lista_coincidencia_final[0].mostrar_producto()
+                                    pregunta_continuar_con_eliminar_el_producto()
+                                    opc = definir_rango_valido_numero_entero(1, 2)
+                                    match opc:
+                                        case 1:
+                                            for indice_producto, producto in enumerate(inventario):
+                                                if lista_coincidencia_final[0].id == producto.id:
+                                                    indice_borrar = indice_producto
+                                            inventario.pop(indice_borrar)
+                                            imprime_producto_eliminado()
+                                            return inventario
+                                        case 2:
+                                            pregunta_continuar_o_salir_nombre_no_encontrado()
+                                            opc = definir_rango_valido_numero_entero(1, 2)
+                                            match opc:
+                                                case 1:
+                                                    pass
+                                                case 2:
+                                                    cancelando()
+                                                    return inventario
+                        case _:
+                            error_match_case()
+            case _:
+                error_match_case()
